de43094c28197f9c810e84f454e4037da9926995
Sean Altemose <sean@seanaltemose.com>
2019-10-01 17:54:36 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,4 @@
 class SongsController < ApplicationController
-  require 'csv'
 
   def index
     @songs = Song.all
@@ -24,9 +23,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
+    LeadsWorker.perform_async(params[:leads].path)
     redirect_to songs_path
   end
 
@@ -59,4 +56,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
